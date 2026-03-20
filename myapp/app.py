746c12e669854a74e41b9ca077c0cf9244316521
Jay Rami <jay.rami@techerudite.com>
2026-03-20 11:11:16 +0530

    added sub fuction to app and test file

--- myapp/app.py
+++ myapp/app.py
@@ -3,5 +3,8 @@
 def add_numbers(a, b):
     return a + b
 
+def subtract_numbers(a, b):
+    return a - b
+
 if __name__ == "__main__":
     print("Sum of 2 and 3 is:", add_numbers(2, 3))
--- myapp/test_app.py
+++ myapp/test_app.py
@@ -8,5 +8,11 @@ class TestApp(unittest.TestCase):
         self.assertEqual(add_numbers(-1, 1), 0)
         self.assertEqual(add_numbers(0, 0), 0)
 
+    def test_subtract_numbers(self):
+        from app import subtract_numbers
+        self.assertEqual(subtract_numbers(5, 3), 2)
+        self.assertEqual(subtract_numbers(0, 1), -1)
+        self.assertEqual(subtract_numbers(10, 5), 5)
+
 if __name__ == "__main__":
     unittest.main()
