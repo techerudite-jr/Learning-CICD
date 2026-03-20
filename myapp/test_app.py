7c416b468d5f304ef07480371c55c9b0aa38a2ef
Jay Rami <jay.rami@techerudite.com>
2026-03-20 11:56:27 +0530

    try to learn confilt

--- myapp/test_app.py
+++ myapp/test_app.py
@@ -4,7 +4,7 @@ from app import add_numbers
 
 class TestApp(unittest.TestCase):
     def test_add_numbers(self):
-        self.assertEqual(add_numbers(2, 3), 5)
+        self.assertEqual(add_numbers(2, 3), 6)  # Simulated conflicting change
         self.assertEqual(add_numbers(-1, 1), 0)
         self.assertEqual(add_numbers(0, 0), 0)
 
