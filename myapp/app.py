09e9872742829af7697ea34bbf5b2dc9b18859a3
Jay Rami <jay.rami@techerudite.com>
2026-03-26 11:18:20 +0530

    added

--- myapp/app.py
+++ myapp/app.py
@@ -1,12 +1,15 @@
 # app.py
 
+
 def add_numbers(a, b):
     return a + b
+
 
 def divide_numbers(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
+
 
 if __name__ == "__main__":
     print("Sum of 2 and 3 is:", add_numbers(2, 3))
