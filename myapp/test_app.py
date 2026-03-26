6cc66ad92ad14e054744d28058d891166d2c605d
Jay Rami <jay.rami@techerudite.com>
2026-03-26 11:22:17 +0530

    added

--- myapp/test_app.py
+++ myapp/test_app.py
@@ -5,7 +5,7 @@ from app import add_numbers
 
 class TestApp(unittest.TestCase):
     def test_add_numbers(self):
-        self.assertEqual(add_numbers(2, 3), 5)  
+        self.assertEqual(add_numbers(2, 3), 5)
         self.assertEqual(add_numbers(-1, 1), 0)
         self.assertEqual(add_numbers(1, 1), 2)
 
@@ -15,6 +15,7 @@ class TestApp(unittest.TestCase):
         self.assertEqual(divide_numbers(10, 2), 5)
         with self.assertRaises(ValueError):
             divide_numbers(5, 0)
+
 
 if __name__ == "__main__":
     unittest.main()
